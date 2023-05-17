d0a9bdd8557bfe6ebb9f5e723a3e7158c2267cef
Ashish <ashishmore2110@gmail.com>
2023-05-17 19:37:57 +0530

    login page  view

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <LogIn />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//import HelloWorld from './components/HelloWorld.vue'
+import LogIn from './components/LogIn'
 
 export default {
-  name: 'App',
+  name: 'App',  
   components: {
-    HelloWorld
+    LogIn
+
   }
 }
 </script>
---
+++ src/components/LogIn.vue
@@ -0,0 +1,31 @@
+<template>
+    <img src="../assets/logo1.png">
+    <h3>LogIn</h3>
+    <div class="authentication">
+        <input type="text" placeholder="Enter Email">
+        <input type="password" placeholder="Enter Password">
+        <button>Log In</button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'LogIn'
+} 
+</script>
+
+<style>
+ .authentication input{
+    width: 300px;
+    height: 40px;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 20px;
+ }
+
+ .authentication button{
+    width: 250px;
+    height: 40px;
+    background-color:  brown;
+ }
+</style>
